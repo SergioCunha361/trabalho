f9d11b09e593dc32ea6a12f17731e6b89f96a501
SergioCunha361 <sergiocunha50@gmail.com>
2024-10-29 21:36:42 -0300

    exercio do MENU incluido

--- index.js
+++ index.js
@@ -1,8 +1,80 @@
-const { criarReceitas } = require  ("./receitas/criarReceitas")
-const { editarReceitas } = require ("./receitas/editarReceitas")
+const prompt = require("prompt-sync")()
+const { criarReceitas } = require("./receitas/criarReceitas")
+const { editarReceitas } = require("./receitas/editarReceitas")
 const { excluirReceitas } = require("./receitas/excluirReceitas")
-const { listarReceitas } = require ("./receitas/listarReceitas")
-const { excluirTodasReceitas } = require ("./receitas/excluirTodas")
+const { listarReceitas } = require("./receitas/listarReceitas")
+const { excluirTodasReceitas } = require("./receitas/excluirTodas")
+
+function exibirMenu() {
+
+    console.log("=====================");
+    console.log("1 - Criar Receita");
+    console.log("2 - Editar Receita")
+    console.log("3 - Excluir Uma Receita")
+    console.log("4 - Listar Receitas")
+    console.log("5 - Excluir todas Receitas")
+    console.log("9 - Sair")
+    console.log("======================");
+
+}
+let id;
+let opcao;
+do {
+    exibirMenu()
+    opcao = parseInt(prompt("Digite número: "));
+
+    switch (opcao) {
+        case 1:
+            id = prompt("Digite o id: ");
+            let nome = prompt("Digite o nome: ");
+            let ingredientes = prompt("Digite ingredientes: ");
+            let passosPreparo = prompt("Digite passos para preparo: ");
+            let tempoPreparo = prompt("Digite tempo de preparo: ");
+            let dificuldades = prompt("Digite dificuldades: ");
+            let porcoes = prompt("Digite porções: ");
+            let autor = prompt("Digite o autor: ");
+            criarReceitas(id, nome, ingredientes, passosPreparo, tempoPreparo, dificuldades, porcoes, autor);
+            break;
+
+        case 2:
+            id = prompt("Digite o id: ");
+            let novoNome = prompt("Digite o novo nome: ");
+            let novoIngredientes = prompt("Digite novo ingredientes: ");
+            let novoPassosPreparo = prompt("Digite o novo passo a passo: ");
+            let novoTempoPreparo = prompt("Digite o novo tempo de preparo: ");
+            let novoDificuldades = prompt("Digite as novas dificuldades: ");
+            let novoPorcoes = prompt("Digite as novas porcões: ");
+            let novoAutor = prompt("Digite o novo autor: ");
+            editarReceitas(id, novoNome, novoIngredientes, novoPassosPreparo, novoTempoPreparo, novoDificuldades, novoPorcoes, novoAutor);
+            listarReceitas();
+            break;
+
+        case 3:
+            id = prompt("Digite id a ser editada: ");
+            excluirReceitas(id);
+            break;
+
+        case 4:
+            listarReceitas();
+            break;
+
+        case 5:
+            excluirTodasReceitas()
+            break;
+
+        case 9:
+            console.log("Saindo do sistema ...")
+            break;
+
+        default:
+            console.log("Valor inválido!")
+            break;
+
+    }
+} while (opcao !== 9);
+
+module.exports = { exibirMenu };
+
 
 //function criarReceitas(id, nome, ingredientes, passosPreparo, tempoPreparo, dificuldades, porcoes, autor)
 
@@ -11,7 +83,7 @@ const { excluirTodasReceitas } = require ("./receitas/excluirTodas")
 
 // criarReceitas(11, "macarronada", "macarrão-óleo-alho-sal", "cozinhar o macarrão na agua com uma colher de òleo ",
 //      "20 min", "fogo medio", "não deixar faltar agua", "Gabriel")
-     
+
 // criarReceitas(15, "ovo frito", "ovo-óleo-sal", "esquentar óleo - colocar o ovo qdo fever óleo",
 //     "1 min", "fogo medio", "não deixar faltar óleo", "Maria")
 
@@ -38,61 +110,4 @@ const { excluirTodasReceitas } = require ("./receitas/excluirTodas")
 // listarAluno()
 
 // excluirAluno("1015")
-// listarAluno()
-
-function exibirMenu() {
-
-    console.log("=====================");
-    console.log("1 - Criar Receita");
-    console.log("2 - Editar Receita")
-    console.log("3 - Excluir Uma Receita")
-    console.log("4 - Listar Receitas")
-    console.log("5 - Excluir todas Receitas")
-    console.log("9 - Sair")
-    console.log("======================");
-
-}
-let id;
-let opcao;
-do {
-    exibirMenu()
-    opcao = parseInt(prompt("Digite número: "));
-
-switch (opcao) {
-    case 1:
-        id = prompt("Digite o id: ");
-        let nome = prompt("Digite o nome: ");
-        let ingredientes = prompt("Digite o email: ");
-        let passosPreparo = prompt("Digite o telefone: ");
-        let tempoPreparo = prompt("Digite o nome: ");
-        let dificuldades = prompt("Digite o email: ");
-        let porcoes = prompt("Digite o telefone: ");
-        let autor = prompt("Digite o telefone: ");
-        criarReceitas(matricula, nome, email, telefone);
-        break;
-    case 2:
-        id = prompt("Digite a matricula a ser editada: ");
-        let novoNome = prompt("Digite o novo nome: ");
-        let novoEmail = prompt("Digite o novo email: ");
-        let novoTelefone = prompt("Digite o novo telefone: ");
-        editarReceitas(matricula, novoNome, novoEmail, novoTelefone);
-        listarReceitas();
-        break;
-    case 3:
-        id = prompt("Digite id a ser editada: ");
-        excluirReceitas(id);
-        break;
-    case 4:
-        listarReceitas();
-        break;
-    case 5:
-        excluirTodasReceitas()
-        break;
-    case 9:
-        console.log("Saindo do sistema ...")
-        break;
-    default:
-        console.log("Valor inválido!")
-        break;
-}
-} while (opcao !== 9);
+// listarAluno();
